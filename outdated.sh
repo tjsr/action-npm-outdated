66957c3e778c72f0f1bbdd38bfefebb323d19f35
Tim Rowe <tim@tjsr.id.au>
2024-07-20 23:43:02 +1000

    Don't print outdated json output when no new version found.

--- outdated.sh
+++ outdated.sh
@@ -80,7 +80,6 @@ VALUES=($(echo $DEPENDENT_DATA | jq -r 'to_entries[] | "\(.key)=\(.value)"'))
 echo $OUTPUT >> "$OUTPUT_TARGET"
 if [ "$(echo $DEPENDENT_DATA | jq -r .hasNewVersion)" != "true" ]; then
   echo "No new version found for $PACKAGE"
-  echo $OUTDATED
   echo "hasNewVersion=false" >> "$OUTPUT_TARGET"
   if [ "$INPUT_FAIL_ON_NO_NEW_VERSION" = "true" ]; then
     exit 1
